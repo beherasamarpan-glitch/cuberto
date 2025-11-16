77f1b5c2a3467b5c6f75cef1b47695c6c2120f7b
beherasamarpan-glitch <beherasamarpan@gmail.com>
2025-11-16 23:25:10 +0530

    website made responsive

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+
 Shery.mouseFollower();
 Shery.makeMagnet(".magnet");
+
+
 Shery.hoverWithMediaCircle(".hvr", {
     videos: ["./1.mp4", "./2.mp4", "./3.mp4"],
 });
 
+-
 gsap.to(".fleftelm", {
     scrollTrigger: {
         trigger: "#fimages",
@@ -14,22 +18,43 @@ gsap.to(".fleftelm", {
         scrub: 1,
     },
     y: "-300%",
-    ease: Power1,
+    ease: "power1",
 });
 
 let sections = document.querySelectorAll(".fleftelm");
+
+
 Shery.imageEffect(".images", {
     style: 4,
     slideStyle: (setScroll) => {
-        sections.forEach(function (section,index) {
+        sections.forEach((section, index) => {
             ScrollTrigger.create({
                 trigger: section,
                 start: "top top",
                 scrub: 1,
-                onUpdate: function (prog) {
+                onUpdate: function(prog) {
                     setScroll(prog.progress + index);
-                },
+                }
             });
         });
     },
 });
+
+//MOBILE FIXES 
+
+
+if (window.innerWidth < 768) {
+    ScrollTrigger.getAll().forEach(t => t.kill());
+
+    
+    gsap.utils.toArray(".fleftelm").forEach((elm) => {
+        gsap.from(elm, {
+            opacity: 0,
+            y: 50,
+            duration: 1,
+            scrollTrigger: {
+                trigger: elm,
+            }
+        });
+    });
+}
